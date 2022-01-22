cd98135d12e4ce21c40d01a5072f34e754a20822
Bahadır Ünal <92628464+ZeroToHero2@users.noreply.github.com>
2022-01-22 18:35:51 +0300

    Delete CMPE EnSON directory

--- CMPE EnSON/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.cmpe313 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires com.jfoenix;
-    // requires org.kordamp.bootstrapfx.core;
-    opens com.example.cmpe313 to javafx.fxml;
-    exports com.example.cmpe313;
-}
