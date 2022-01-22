df128deecad7029ca8dfd08c9c9424bf2785c5fb
Bahadır Ünal <92628464+ZeroToHero2@users.noreply.github.com>
2022-01-22 18:42:48 +0300

    Update SceneController.java

--- Project/src/main/java/com/example/cmpe313/SceneController.java
+++ Project/src/main/java/com/example/cmpe313/SceneController.java
@@ -3,7 +3,6 @@ package com.example.cmpe313;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -19,8 +18,6 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-
-
 
 
 public class SceneController { //implements Initializable
